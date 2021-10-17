971d778994e9578380fe813012490d13b2057222
modernscape <modernscape@gmail.com>
2021-10-17 17:47:14 +0900

    21-10-17_

---
+++ data.js
@@ -0,0 +1,19 @@
+var exItems = [
+  {
+    id: 1,
+    title: '仏道',
+    comment: '心を清らかにして安穏に達する',
+    time: '2:10',
+    executing: true
+  },
+  {
+    id : 2,
+    title: 'レバリ',
+    comment: '世の中の問題に貢献する',
+    time: '',
+    executing: false
+  },
+];
+
+
+
